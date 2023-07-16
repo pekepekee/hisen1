0da75a8c909b6abbe61bc193a52af04317a697e4
pekepekee <80317395+pekepekee@users.noreply.github.com>
2023-07-16 20:58:51 +0900

    Add files via upload

---
+++ index.js
@@ -0,0 +1,32 @@
+var app = require("express")();
+var http = require("http").createServer(app);
+var io = require("socket.io")(http);
+var fs = require("fs");
+
+app.get("/", (req, res) => {
+  res.sendFile(__dirname + "/index.html");
+});
+
+io.on("connection", (socket) => {
+  var socketId = socket.id;
+  var roomId = socket.handshake.query.id;
+  socket.join(roomId);
+  try {
+    var data = fs.readFileSync(`data/${roomId}.json`, "utf-8");
+    io.to(socketId).emit("server_item_update", JSON.parse(data));
+  } catch (e) {
+    console.log(e);
+  }
+  socket.on("client_item_update", (data) => {
+    socket.broadcast.to(roomId).emit("server_item_update", data);
+    fs.writeFile(`data/${roomId}.json`, JSON.stringify(data), (err) => {
+      if (err) throw err;
+    });
+  });
+});
+
+const PORT = process.env.PORT || 3000;
+
+http.listen(PORT, () => {
+  console.log("listening on *:3000");
+});
